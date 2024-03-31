e940208fda81d5c607860d8dc170cfe76c2c391b
SudhakarBadigar <sudhakar.badigar87@gmail.com>
2024-03-31 10:24:56 +0530

    dsa

--- java8/src/predefinedFunctionalIntefrace/PredicateJoining2.java
+++ java8/src/predefinedFunctionalIntefrace/PredicateJoining2.java
@@ -4,7 +4,7 @@ import java.util.function.Predicate;
 
 public class PredicateJoining2 {
 	public static void main(String[] args) {
-		int y[]= {0,3,5,6,8,10,12,15,18,21,22};
+		int y[]= {0,3,5,6,8,10,12,15,18,21,22,24,25,26};
 		Predicate<Integer> p1 = e-> e%2==0;
 		Predicate<Integer> p2 = e->e>10;
 		for(Integer i:y) {
